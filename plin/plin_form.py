408f2b4968501a9ee123f7a22d829f650a229588
lennyQL <Lenny_Zhu@kyurin-no-MacBook-Air.local>
2017-02-13 02:38:48 +0900

    main window

--- plin/plin_form.py
+++ plin/plin_form.py
@@ -1,1 +1,29 @@
-#Plin's GUI
+#<=====Plin's GUI=====>
+
+import tkinter as tk
+
+###############
+# interface
+###############
+
+def run():
+# global variable
+    global enter, response_area, lb, action
+
+# main window
+    root = tk.Tk()
+# window size
+    root.geometry("880x560")
+# window title
+    root.title("Chat Bot : PLIN")
+#  font
+    font = ("Helevetica", 14)
+    font_log = ("Helevetica", 11)
+
+
+# main loop
+    root.mainloop()
+
+#*****main program*****
+if __name__ == "__main__":
+    run()
