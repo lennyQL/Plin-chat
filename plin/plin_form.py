cf5ea6e744de8de63e23b92762c21b45c027015c
lennyQL <Lenny_Zhu@kyurin-no-MacBook-Air.local>
2017-02-14 00:10:24 +0900

    made menubar

--- plin/plin_form.py
+++ plin/plin_form.py
@@ -16,9 +16,31 @@ def run():
     root.geometry("880x560")
 # window title
     root.title("Chat Bot : PLIN")
-#  font
+# font
     font = ("Helevetica", 14)
-    font_log = ("Helevetica", 11)
+    font_log = ("Helevetica", 12)
+
+# menu bar
+    menubar = tk.Menu(root)
+    root.config(menu=menubar)
+# 「ファイル」menu
+    filemenu = tk.Menu(menubar)
+    menubar.add_cascade(label="ファイル", menu=filemenu)
+    filemenu.add_command(label="閉じる", command=root.destroy)
+# 「オプション」menu
+    action = tk.IntVar()
+    optionmenu = tk.Menu(menubar)
+    menubar.add_cascade(label="オプション", menu=optionmenu)
+    optionmenu.add_radiobutton(
+        label="Responderを表示",  #アイテム名
+        variable = action,       #選択時の値を格納するオブジェクト
+        value = 0                #actionの値を0にする
+    )
+    optionmenu.add_radiobutton(
+        label="Responderを表示しない",  #アイテム名
+        variable = action,       #選択時の値を格納するオブジェクト
+        value = 1                #actionの値を1にする
+    )
 
 
 # main loop
